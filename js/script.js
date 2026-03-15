483c4c8e6f9eb720dbafff347c4367c4a3e4fe6d
João Victor <victor.victor19@hotmail.com>
2026-03-15 16:43:14 -0300

    1202 parametros URL

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
+//Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-
+  //include busca palavras iguais, por exemplo, o URL ta puxando a tag HTML 'a', que contem o href, se o a variavel "href" possuir href dentro do seu conteudo, é true
   if (url.includes(href)) {
-    link.classList.add("ativo")
+    link.classList.add("ativo");
   }
-
-  console.log(url);
-  console.log(href);
 }
 
 links.forEach(ativarLink);
 
-console.log(links);
+//Ativar itens do orçamento
+
+//new URLSearchParams() retorna array com os parametros de busca
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
